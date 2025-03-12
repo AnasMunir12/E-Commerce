4c6fe40d2ae9b07f63b01d09e1fad2cfbd726249
AnasMunir12 <anassheik890@gmail.com>
2025-03-13 04:08:07 +0500

    Second Commit

---
+++ src/Components/Banner.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+import Slider from "react-slick";
+import { Box, IconButton, Typography } from "@mui/material"; 
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css"; 
+
+import Iphonerimg from '../images/Bannerimg.png'
+import Samsungimg from '../images/samsungbanner.jpg'
+import Oneplusimg from '../images/oneplusbanner.jpg'
+import AppleIcon from '@mui/icons-material/Apple';
+import AndroidIcon from '@mui/icons-material/Android';
+import SmartphoneIcon from '@mui/icons-material/Smartphone';
+export default function Banner() {
+    const settings = {
+        dots: true,         
+        infinite: true,    
+        speed: 500,         
+        slidesToShow: 1,    
+        slidesToScroll: 1,  
+        autoplay: true,     
+        autoplaySpeed: 2000, 
+        arrows: true    
+      };
+
+      const Banners = [
+        {icon : <AppleIcon />,
+        name:"iPhone 14 Series",
+        img: Iphonerimg,
+        },
+        {icon : <AndroidIcon />,
+            name:"Samsung Galaxy S23",
+            img:  Samsungimg,
+
+            },
+        {icon : <SmartphoneIcon />,
+                name:"OnePlus 11",
+                img: Oneplusimg,
+                },
+
+      ]
+
+  return (
+    <>
+<Box sx={{ width: "80%",height:"350px", margin: "auto", mt: 4, background:"white" }} >
+<Slider {...settings}>
+{Banners.map((banner , index) => (
+    <Box display={"flex"} flexDirection={"row"}>
+        <Box 
+        key={index}
+        display={"flex"}
+        flexDirection={"row"}
+        alignItems={"center"}
+        > 
+            <IconButton>{banner.icon}</IconButton>
+            <Typography>{banner.name}</Typography>
+        </Box>
+        <Box 
+        component="img"
+        src={banner.img}
+        alt={banner.name}
+        sx={{
+            width:"400px",
+            height:"300px"
+            
+        }}
+        >
+      
+        </Box>
+    </Box>
+))}
+
+</Slider>
+    </Box>
+    </>
+  )
+}
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,16 +1,77 @@
-import { Box, Stack, Typography } from '@mui/material'
-import React from 'react'
+import { Box, IconButton, Input, Link, Stack, Typography } from "@mui/material";
+import React from "react";
+
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
 
 export default function Navbar() {
+  const navLinks = [
+    { name: "Home", path: "/" },
+    { name: "Contact", path: "/" },
+    { name: "About", path: "/" },
+    { name: "Sign Up", path: "/" },
+  ];
+
   return (
     <>
-      <Box>
-            <Box>
-                <Stack>
-                    {/* <Typography>Exclusive</Typography> */}
-                </Stack>
-            </Box>
+      <Box width={"100%"} height={"20px"} p={3}>
+        <Box
+          display={"flex"}
+          alignItems={"center"}
+          justifyContent={"space-around"}
+        >
+          <Stack>
+            <Typography sx={{ fontWeight: 700 }} variant="h6">
+              {" "}
+              Exclusive
+            </Typography>
+          </Stack>
+          <Stack display={"flex"} flexDirection={"row"} gap={"40px"}>
+            {navLinks.map((link) => (
+              <Link
+                to={link.path}
+                href="#"
+                underline="hover"
+                sx={{ fontSize: "small", color: "black" }}
+              >
+                {link.name}
+              </Link>
+            ))}
+          </Stack>
+          <Stack display={"flex"} flexDirection={"row"} alignItems={"center"}>
+            <Stack
+              width={"auto"}
+              height={"35px"}
+              sx={{ background: "#F5F5F5", borderRadius: "6px" }}
+              display={"flex"}
+              flexDirection={"row"}
+              px={"8px"}
+            >
+              <Input
+                placeholder='What are you lookign for?"'
+                disableUnderline
+                sx={{
+                  fontSize: "small",
+                  "& : placeholder": { fontSize: "xs" },
+                }}
+              />
+              <IconButton>
+                <SearchOutlinedIcon fontSize="small" />
+              </IconButton>
+            </Stack>
+            <IconButton>
+              <FavoriteBorderOutlinedIcon fontSize="small" />
+            </IconButton>
+            <IconButton>
+              <ShoppingCartOutlinedIcon fontSize="small" />
+            </IconButton>
+          </Stack>
+        </Box>
       </Box>
+      <Stack
+        sx={{ border: " 0.5px solid #000000", opacity: "10%", transform:"rotate(-180deg)" }}
+      ></Stack>
     </>
-  )
+  );
 }
