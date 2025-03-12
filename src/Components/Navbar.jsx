2f0faf289034e0018ceed1b3d301626f57aefca5
AnasMunir12 <anassheik890@gmail.com>
2025-03-12 23:36:58 +0500

    First commit

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,16 @@
+import { Box, Stack, Typography } from '@mui/material'
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <>
+      <Box>
+            <Box>
+                <Stack>
+                    {/* <Typography>Exclusive</Typography> */}
+                </Stack>
+            </Box>
+      </Box>
+    </>
+  )
+}
